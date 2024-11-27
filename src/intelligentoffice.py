f34f5aac88b252a40352cf65356cbd2e58da7047
katiaperchet <katiamperchet@gmail.com>
2024-11-27 16:15:35 +0100

    green phase US1

--- src/intelligentoffice.py
+++ src/intelligentoffice.py
@@ -54,9 +54,13 @@ class IntelligentOffice:
         self.buzzer_on = False
 
     def check_quadrant_occupancy(self, pin: int) -> bool:
-        if GPIO.input(pin) == GPIO.HIGH:
-            return True
-        return False
+        if pin in [self.INFRARED_PIN1, self.INFRARED_PIN2, self.INFRARED_PIN3, self.INFRARED_PIN4]:
+            if GPIO.input(pin) == GPIO.HIGH:
+                return True
+            else:
+                return False
+        else:
+            raise IntelligentOfficeError()
     def manage_blinds_based_on_time(self) -> None:
         # To be implemented
         pass
